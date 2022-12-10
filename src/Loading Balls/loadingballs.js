5784bbe952d3a06b73f31bd875c6e6cc03e36097
Keiryan Wilson <keiryanwilson@gmail.com>
2022-12-09 20:49:25 -0500

    fixed console error

--- src/Loading Balls/loadingballs.js
+++ src/Loading Balls/loadingballs.js
@@ -1,22 +1,25 @@
 import {
-  ContainerForBalls,
-  BallAndShadowContainer,
-  Ball,
-  BallShadow,
-} from "./styles.loadingballs.js";
-
-export default function LoadingBalls({ ballAmount }) {
-  const balls = new Array(ballAmount || 3).fill({ id: Math.random() });
-  return (
-    <ContainerForBalls>
-      {balls.map((ball, index) => {
-        return (
-          <BallAndShadowContainer key={ball.id}>
-            <Ball index={index} />
-            <BallShadow index={index} />
-          </BallAndShadowContainer>
-        );
-      })}
-    </ContainerForBalls>
-  );
-}
+    ContainerForBalls,
+    BallAndShadowContainer,
+    Ball,
+    BallShadow,
+  } from "./styles.loadingballs.js";
+  
+  const newId = () => Math.random();
+  
+  export default function LoadingBalls({ ballAmount }) {
+    const balls = new Array(ballAmount || 3).fill('ball');
+    return (
+      <ContainerForBalls>
+        {balls.map((ball, index) => {
+          return (
+            <BallAndShadowContainer key={newId()}>
+              <Ball index={index} />
+              <BallShadow index={index} />
+            </BallAndShadowContainer>
+          );
+        })}
+      </ContainerForBalls>
+    );
+  }
+  
